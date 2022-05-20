feccffc3e126f395f5182aa0e1c0cafc2367c223
Renaldo Paulo <eiryrenaldo4@gmail.com>
2022-05-20 15:53:36 +0100

    mudei o background e apaguei o button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function MyButton() {
-  return (<button>Clica-me</button>);
-}
 
 function App() {
   return (
     <div>
     <h1>Seja bem vindo a minha Aplicação</h1>
-    <MyButton/>
-    <MyButton/>
-    <MyButton/>
     </div>
   );
 }
