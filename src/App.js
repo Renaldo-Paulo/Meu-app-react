482adf10662002b0e504aab5014f85aed6ad17dc
Renaldo Paulo <eiryrenaldo4@gmail.com>
2022-05-20 16:29:34 +0100

    criei novamente os botões

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 
+//Vamos criar um componente
+
+function MyButton() {
+  return (
+    <button>Clica-me</button>
+  );
+}
+
 function App() {
   return (
     <div>
     <h1>Seja bem vindo a minha Aplicação</h1>
+    
+    <MyButton/>
+    <MyButton/>
+    <MyButton/>
+    <MyButton/>
     </div>
   );
 }
